865b5d45b873361f3a752bc20ccc866ebaceb486
Roma Zhyhulskyi <Roma Zhyhulskyi>
2017-05-06 20:48:27 +0300

    asa

--- public/sportstore/controllers/sportsStore.js
+++ public/sportstore/controllers/sportsStore.js
@@ -1,6 +1,7 @@
 angular.module("sportsStore")
 .constant('dataUrl', "http://localhost:2403/products")
-.controller('sportsStoreCtrl', function($scope, $http, dataUrl){
+.constant('orderUrl', "http://localhost:2403/orders")
+.controller('sportsStoreCtrl', function($scope, $http, $location, dataUrl, orderUrl, cart){
 	
 	$scope.data = {};
 
@@ -12,6 +13,24 @@ angular.module("sportsStore")
 
 	function rejected(error) {
 		$scope.data.error = error;
+	}
+
+	$scope.sendOrder = function (shippingDetails) {
+		var order = angular.copy(shippingDetails);
+		order.products = cart.getProducts();
+		$http.post(orderUrl, order).then(orderFullfiled, orderRejected).finally(function () {
+			$location.path("/complete");
+		});
+
+	}
+
+	function orderFullfiled(response) {
+		$scope.data.orderId = response.data.id;
+		cart.getProducts().length = 0;
+	}
+
+	function orderRejected(error) {
+		$scope.data.orderError = error;
 	}
 
 });
