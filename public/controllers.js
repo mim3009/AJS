b5a17ce96658de0eb13fc01240476aa1685ae4f9
Roma Zhyhulskyi <Roma Zhyhulskyi>
2017-05-07 22:03:48 +0300

    aaa

---
+++ public/controllers.js
@@ -0,0 +1,7 @@
+angular.module('exampleApp.Controllers', [])
+.controller('dayCtrl', function ($scope, days) {
+	$scope.day = days.today;
+})
+.controller('tomorrowCtrl', function ($scope, days) {
+	$scope.day = days.tomorrow;
+});
